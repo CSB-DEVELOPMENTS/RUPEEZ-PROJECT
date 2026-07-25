8647f8372d53439374cd310fdfe2505bc17f84d4
Ayesha Pemal <ayeshapemal@gmail.com>
2026-07-25 12:51:51 +0530

    feat: add database types definition for JSON structure

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,35 +3,36 @@ import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 import { Platform } from "react-native";
+import { Database } from "../../database.types";
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabasePublishableKey = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
 
 export const supabaseConfigError =
-  !supabaseUrl || !supabasePublishableKey
-    ? "Missing EXPO_PUBLIC_SUPABASE_URL or EXPO_PUBLIC_SUPABASE_ANON_KEY."
-    : null;
+  !supabaseUrl || !supabasePublishableKey ?
+    "Missing EXPO_PUBLIC_SUPABASE_URL or EXPO_PUBLIC_SUPABASE_ANON_KEY."
+  : null;
 
 const isBrowser = typeof window !== "undefined";
 
 const storage =
-  Platform.OS === "web"
-    ? isBrowser
-      ? {
-          getItem: (key: string) => Promise.resolve(window.localStorage.getItem(key)),
-          setItem: (key: string, value: string) => {
-            window.localStorage.setItem(key, value);
-            return Promise.resolve();
-          },
-          removeItem: (key: string) => {
-            window.localStorage.removeItem(key);
-            return Promise.resolve();
-          },
-        }
-      : undefined
-    : AsyncStorage;
+  Platform.OS === "web" ?
+    isBrowser ?
+      {
+        getItem: (key: string) => Promise.resolve(window.localStorage.getItem(key)),
+        setItem: (key: string, value: string) => {
+          window.localStorage.setItem(key, value);
+          return Promise.resolve();
+        },
+        removeItem: (key: string) => {
+          window.localStorage.removeItem(key);
+          return Promise.resolve();
+        },
+      }
+    : undefined
+  : AsyncStorage;
 
-export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
+export const supabase = createClient<Database>(supabaseUrl, supabasePublishableKey, {
   auth: {
     storage,
     autoRefreshToken: isBrowser,
